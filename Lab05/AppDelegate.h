1524d3a0f2741c2a2bf01fccf001dbd69458e1d6
jhamer02 <jhamer02@hotmail.com>
2016-09-01 20:38:39 -0500

    Initial Commit

---
+++ Lab05/AppDelegate.h
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.h
+//  Lab05
+//
+//  Created by Andrea Rosas Pérez on 25/06/16.
+//  Copyright © 2016 jjpe. All rights reserved.
+//
+
+/***Simón, es una prueba****/
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
